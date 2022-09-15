4ed8d3dd73f89fd2f727267c473a448da656d996
joaodordio <joaomigueldordio@gmail.com>
2022-09-15 20:21:56 +0100

    Initial Commit

---
+++ Trimmer 9000/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trimmer 9000
+//
+//  Created by Joao Dordio on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
